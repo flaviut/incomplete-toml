2d5c9e9930edc708e7dad3608d7a43f95563347e
Flaviu Tamas <tamasflaviu@gmail.com>
2015-01-05 21:15:12 -0500

    Add tests for strings tokenization

--- test/tlex.nim
+++ test/tlex.nim
@@ -8,9 +8,33 @@ proc tc(str: string): LexContext =
 suite "string matching":
   test "raw strings":
     check(tc("'\\u1234'").matchString().get == "\\u1234")
+    check(tc("'\\'").matchString().get == "\\")
+    check(tc("'\\'").matchString().get == "\\")
+  test "escape codes":
+    check(tc("\"\\t\"").matchString().get == "\t")
+    check(tc("\"\\r\"").matchString().get == "\r")
+    check(tc("\"\\n\"").matchString().get == "\l")
+  test "multiline":
+    check(tc("\"\"\"\l\l\"\"\"").matchString().get == "\l\l")
+    check(tc("\"\"\"\r\r\"\"\"").matchString().get == "\r\r")
+  test "multiline raw strings":
+    check(tc("'''\l\l'''").matchString().get == "\l\l")
+    check(tc("'''\r\r'''").matchString().get == "\r\r")
   test "unicode":
     check(tc("\"\\u00A5\\u39F4\"").matchString().get == "¥㧴")
-    check(tc("\"\\u00010485\"").matchString().get == "𐒅")
+    check(tc("\"\\U00010485\"").matchString().get == "𐒅")
+  test "unoffical unicode syntax extentions":
+    check(tc("\"\\u10485\"").matchString().get == "𐒅")
+    check(tc("\"\\u1BB\"").matchString().get == "ƻ")
+
+  test "syntax errors":
+    expect(SyntaxError): discard tc("'\r'").matchString
+    expect(SyntaxError): discard tc("\"\l\"").matchString
+    expect(SyntaxError): discard tc("'\x01'").matchString
+    expect(SyntaxError): discard tc("'\x00'").matchString
+    expect(SyntaxError): discard tc("\"\x00\"").matchString
+
+
 
 # proc matchDateTime(ctx: LexContext): Option[DateTime]
 # proc matchBool(ctx: LexContext): Option[bool]
